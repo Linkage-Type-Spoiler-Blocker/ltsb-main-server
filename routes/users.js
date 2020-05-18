ef66f4a8ddb6e54a8193d66f721fa55e420979e0
박세지 <thswlrud0@gmail.com>
2020-05-18 09:14:52 +0900

    회원가입, 로그인, 비밀번호 암호화, 인증

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = requrie('crypto');
 /* Sign up. */
 // router.post('/signup', function(req, res, next) {
 //   res.send('respond with a resource');
@@ -11,5 +11,23 @@ var router = express.Router();
 // router.post('/login', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
+
+//비밀번호 암호화
+router.post("/sign_up", async function(req,res,next){
+  let body = req.body;
+
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  let hashPassword = crypto.createHash("sha256").update(inputPassword + salt).digest("hex");
+
+  let result = models.user.create({
+    email: body.userEmail,
+    password: hashPassword,
+    salt: salt
+  })
+
+  res.redirect("/route/index");
+})
+
 
 module.exports = router;
