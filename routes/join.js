847be730c8156f8d3d24ff94ac46ee4e38da2ff5
백준하 <astoru@naver.com>
2020-06-01 19:10:32 +0900

    using const instead of var

--- routes/join.js
+++ routes/join.js
@@ -1,30 +1,30 @@
-var express = require('express')
- var app = express()
- var router = express.Router()
- var path = require('path')
- var mysql = require('mysql')
+const express = require('express')
+const app = express()
+const path = require('path')
+const mysql = require('mysql')
 
+const router = express.Router()
 
- var connection = mysql.createConnection({
-      host     : 'localhost',
-      port     : 3000,
-      user     : /*입력*/,
-      password : /*입력*/,
-      database : /*입력*/'
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3000,
+    user     : /*입력*/,
+    password : /*입력*/,
+    database : /*입력*/'
 });
 
 router.get('/', function(req,res){
      res.sendFile(path.join(__dirname, './route/index'))
  })
 
- router.post('/', function(req,res){
-     var email = body.email;
-     var passwd = body.password;
+router.post('/', function(req,res){
+ var email = body.email;
+ var passwd = body.password;
 
-     var query = connection.query('insert into user (email, password) values ("' + email + '","' + passwd + '")', function(err, rows) {
-         if(err) { throw err;}
-         console.log("Data inserted!");
-     })
+ var query = connection.query('insert into user (email, password) values ("' + email + '","' + passwd + '")', function(err, rows) {
+     if(err) { throw err;}
+     console.log("Data inserted!");
  })
+})
 
- module.exports = router;
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,44 @@
-var express = require('express');
-var router = express.Router();
-var crypto = require('crypto');
-/* Sign up. */
-// router.post('/signup', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+const express = require('express');
 
+const crypto = require('crypto');
 
-/* Sign in. */
-// router.post('/login', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+const router = express.Router();
+const { registration } = require('../controllers');
+
+router.post('/signup', registration.applyRegistration);
+
+router.post('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/check-email', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/recovery', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/backup', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 //비밀번호 암호화
-router.post("/sign_up", async function(req,res,next){
-  let body = req.body;
-
-  let inputPassword = body.password;
-  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
-  let hashPassword = crypto.createHash("sha256").update(inputPassword + salt).digest("hex");
-
-  let result = models.user.create({
-    email: body.userEmail,
-    password: hashPassword,
-    salt: salt
-  })
-
-  res.redirect("/route/index");
-})
+// router.post("/sign_up", async function(req,res,next){
+//   let body = req.body;
+//
+//   let inputPassword = body.password;
+//   let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+//   let hashPassword = crypto.createHash("sha256").update(inputPassword + salt).digest("hex");
+//
+//   let result = models.user.create({
+//     email: body.userEmail,
+//     password: hashPassword,
+//     salt: salt
+//   })
+//
+//   res.redirect("/route/index");
+// })
 
 
 module.exports = router;
