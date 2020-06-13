04ff431426d6beb20a49e971d9a88447ce8c6050
백준하 <astoru@naver.com>
2020-06-13 20:02:48 +0900

    영화 리스트 받아오기 테스트 완료

--- services/awsConnector.js
+++ services/awsConnector.js
@@ -3,16 +3,14 @@
 const env = process.env.NODE_ENV ||'development';
 
 const AWS = require('aws-sdk');
-
-const loadS3 = async()=>{
-    AWS.config.region = 'us-west-1';
-    if(env === 'development'||'test'){
-        AWS.config.accessKeyId = process.env.AWS_DEV_ACCESS_KEY;
-        AWS.config.secretAccessKey = process.env.AWS_DEV_SECRET_ACCESS_KEY;
-    }
-
-    return new AWS.S3();
+if (env === 'development' || 'test') {
+    AWS.config.accessKeyId = process.env.AWS_DEV_ACCESS_KEY;
+    AWS.config.secretAccessKey = process.env.AWS_DEV_SECRET_ACCESS_KEY;
 }
+const S3 = new AWS.S3();
+
+console.log('S3 is'+ typeof S3);
+console.log(Object.keys(S3));
 
 const loadSES = async()=>{
     const sesCred = await AWS.config.loadFromPath('../config/ses-config.json');
@@ -20,10 +18,11 @@ const loadSES = async()=>{
 }
 
 //TODO 테스트 필요.
-(async()=>{
-    exports.S3 = await loadS3();
-    // exports.SES = await loadSES();
-})();
+// (async()=>{
+//     exports.S3 = await loadS3();
+//     // exports.SES = await loadSES();
+// })();
 
 module.exports = {
+    S3 : S3
 }
